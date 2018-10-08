a4694b8ba9c10b38870344836da7fd2987251aa9
sonubdu <9015910967sonu@gmail.com>
2018-10-08 16:48:14 +0530

    description change

--- src/components/Productdisciption.js
+++ src/components/Productdisciption.js
@@ -27,21 +27,21 @@ class Productdiscription extends Component {
             <div className="col-lg-7">
               <h2 className="discriptionTittle red">Premium Materials</h2>
               <p className="discriptionText">
-                The iPhone XR is here - an affordable iPhone with a premium
+                The Desinger skins is here - an affordable Phone with a premium
                 glass-back design. You know what that means, right? Fingerprints
                 galore and a high probability it'll slip out of your hands. This
-                is where dbrand iPhone XR skins come in. What better way to
-                resolve both issues than with a dbrand iPhone XR skin? As the
+                is where Robobull Desinger  skins come in. What better way to
+                resolve both issues than with a Robobull Desinger  skin? As the
                 world leader in smartphone skins, we of course partnered with
                 the world leader in skin material - 3M. We're the only company
                 to use authentic 3M materials across our entire portfolio of
-                iPhone XR skins. What does that mean for you? A guarantee that,
-                when you're ready to swap out your iPhone XR wrap with another
+                Desinger  skins. What does that mean for you? A guarantee that,
+                when you're ready to swap out your mobile  wrap with another
                 one, it's guaranteed to come off without any adhesive residue or
                 damage to your device. Thanks to our massive buying power with
-                3M, dbrand is able to bring you industry-exclusives like Black
-                Camo iPhone XR skins and the super-grippy Black Dragon. iPhone
-                XR skins from dbrand: guaranteed to protect your device from
+                3M, Robobull is able to bring you industry-exclusives like Black
+                Camo Desinger  skins and the super-grippy Black Dragon. Phone
+                 skins from Robobull: guaranteed to protect your device from
                 scratches and make fingerprints a thing of the past.
               </p>
             </div>
@@ -52,20 +52,20 @@ class Productdiscription extends Component {
             <div className="col-lg-7">
               <h2 className="discriptionTittle green">Unrivaled Precision</h2>
               <p className="discriptionText">
-                At dbrand, precision isn't just a marketing buzzword. It's the
-                only way we know. When it comes to the fit on dbrand iPhone XR
+                At Robobull, precision isn't just a marketing buzzword. It's the
+                only way we know. When it comes to the fit on Robobull Desinger 
                 skins, perfection is the only measure of success. We expect
                 nothing less and neither should you. If you're searching for the
-                best iPhone XR skin on the planet, look no further. Our iPhone
-                XR skins go through a painstakingly meticulous prototyping
-                process - over a thousand iterations to ensure that your iPhone
-                XR wrap is absolutely flawless. It's this absolute attention to
+                best Desinger  skin on the planet, look no further. Our Phone
+                 skins go through a painstakingly meticulous prototyping
+                process - over a thousand iterations to ensure that your Phone
+                wrap is absolutely flawless. It's this absolute attention to
                 detail which has earned us the reputation for the most precise
-                fit on earth™. If you're looking to protect your iPhone XR from
+                fit on earth™. If you're looking to protect your mobile from
                 fingerprints and add some grip, all while adding zero bulk,
                 you've come to the right place. It literally isn't possible to
-                find a higher quality, more precise iPhone XR wrap than the one
-                you'll find here at dbrand. Stop reading and give us your money.
+                find a higher quality, more precise mobile wrap than the one
+                you'll find here at Robobull. Stop reading and give us your money.
               </p>
             </div>
             <div className="col-lg-5">
@@ -91,22 +91,22 @@ class Productdiscription extends Component {
               <p className="discriptionText">
                 Common sense would tell you that, when you buy something, you
                 should be able to see exactly what it looks like before forking
-                over your hard-earned money. Unfortunately, the iPhone XR skin
+                over your hard-earned money. Unfortunately, the Desinger  skin
                 game is riddled with poorly photoshopped representations of the
-                product. Not at dbrand. Since 2011, we've been leading the pack
+                product. Not at Robobull. Since 2011, we've been leading the pack
                 with our one-of-a-kind skin customization interface. A product
-                preview where you can see exactly what your iPhone XR skin will
+                preview where you can see exactly what your Desinger  skin will
                 look like before making the purchase. Go ahead - scroll up and
                 check it out. From Matte Black to Marble, Black Camo to Carbon
-                Fiber, even Mahogany and Bamboo - we've got an iPhone XR skin
+                Fiber, even Mahogany and Bamboo - we've got an Desinger  skin
                 that's sure to lighten up your wallet. Skeptical? We don't blame
                 you - that's why we take world-class product photography to give
-                you an actual real-life look at the iPhone XR skins shown on our
+                you an actual real-life look at the Desinger  skins shown on our
                 customizer. As expected, what you see is what you get: legendary
-                precision and true textured materials. Customizing your iPhone
-                XR has never been this fun - or easy. Oh, and in case you were
+                precision and true textured materials. Customizing your mobile
+                 has never been this fun - or easy. Oh, and in case you were
                 wondering... yes - every purchase comes with both a full body
-                and glass-coverage iPhone XR skin at no additional cost.
+                and glass-coverage mobile skin at no additional cost.
               </p>
             </div>
           </div>
